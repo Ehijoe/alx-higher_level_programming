16f3a52b6108fb9a6458ecf82efb87c5dfd90094
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-20 16:10:57 +0100

    0x01 - Fix task 1 typo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,6 +13,6 @@ value = "and is less than 6 and not 0"
 if last_digit > 5:
     value = "and is greater than 5"
 elif last_digit == 0:
-    value = "and is zero"
+    value = "and is 0"
 
 print(f"Last digit of {number} is {last_digit} {value}")
