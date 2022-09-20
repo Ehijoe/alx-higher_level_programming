6e793ba3921369d5d2950b2fab4aaa194f64d8ba
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-20 15:56:36 +0100

    0x01 - Fix task 1 for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,10 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+last_digit = int(str(number)[-1])
+
+if number < 0:
+    last_digit = 0 - last_digit
 
 value = "and is less than 6 and not 0"
 
