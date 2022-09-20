d72e5eda57c887b69373ec6860a58ae15334f01f
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-20 19:20:19 +0100

    0x01 - Fix task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,8 @@
 
 def print_last_digit(number):
     """Print the last digit of a number."""
+    if not type(number) is int:
+        raise TypeError("Only integers are allowed")
     digit = str(number)[-1]
     print(digit, end="")
     return digit
