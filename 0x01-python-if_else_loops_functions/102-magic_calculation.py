9146b082586b64f2014f688a734e1a0b0e6b551a
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-20 18:29:45 +0100

    0x01 - Add task 102

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A magical function."""
+
+
+def magic_calculation(a, b, c):
+    """Do something magical."""
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return (a * b) - c
