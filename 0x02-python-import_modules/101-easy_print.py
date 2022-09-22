d3b984afbd334f5e1bb78ec32229853c060122a9
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-22 13:35:40 +0100

    0x02 - Add task 101

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Proclaim pythons coolness."""
 
-from sys import stdout
+import os
 
 if __name__ == '__main__':
-    stdout.write("#pythoniscool\n")
+    os.write(1, "#pythoniscool\n")
