2fc81c716244103a670623914ac9baaef6b4cc96
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-22 12:29:42 +0100

    0x02 - Fix task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,5 +5,4 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    value = f"{a} + {b} = {add(a, b)}"
-    print(value, end="\n")
+    print("{} + {} = {}".format(a, b, add(a, b)))
