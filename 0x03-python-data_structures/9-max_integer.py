c2495fdb669452b469f309fdccb90d21d7b8e1fe
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-23 23:49:27 +0100

    0x03 - Add task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Find the max."""
+
+
+def max_integer(my_list=[]):
+    """Return the biggest integer in a list."""
+    if len(my_list) == 0:
+        return None
+    max_ = my_list[0]
+    for num in my_list[1:]:
+        if num > max_:
+            max_ = num
+    return max_
