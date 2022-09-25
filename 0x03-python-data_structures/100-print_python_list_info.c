4127f463525ebb07949fdefabc49d9fd4e13ec0e
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-25 22:56:09 +0100

    0x03 - Add task 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,34 @@
+define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include <object.h>
+#include <listobject.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_python_list_info - Prints information about a python list
+ * @p: A pointer to the list object
+ */
+void print_python_list_info(PyObject * p)
+{
+	int len, i;
+
+	if (!PyList_Check(p))
+	{
+		printf("This is not a list\n");
+		exit(1);
+	}
+
+	len = (int) PyList_Size(p);
+	printf("[*] Size of the Python List = %d\n", len);
+	printf("[*] Allocated = %d\n", (int) Py_SIZE(p));
+
+	for (i = 0; i < len; i++)
+	{
+		printf("Element %d: %s\n",
+		       i,
+		       Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
+}
