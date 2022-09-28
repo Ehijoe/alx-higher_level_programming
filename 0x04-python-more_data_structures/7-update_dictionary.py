82c9bf0b00e375f0ec49dd11fe821c0b2507c3ff
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-28 15:10:17 +0100

    0x04 - Add task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Update dictionary."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Update or add a key-value pair to a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
