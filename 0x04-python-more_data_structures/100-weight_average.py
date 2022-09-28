3b51c954f7ac8af0e2092f47b1e38ee76894cc5c
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-28 16:36:49 +0100

    0x04 - Fix task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,4 +4,6 @@
 
 def weight_average(my_list=[]):
     """Return the weighted average given a list of the weights and scores."""
-    return sum([a[0] * a[1] for a in my_list]) / sum(a[0] for a in my_list)
+    if len(my_list) == 0:
+        return 0
+    return sum([a[0] * a[1] for a in my_list]) / sum(a[1] for a in my_list)
