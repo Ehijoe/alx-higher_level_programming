9de59722ae8b3356b46f114f115ad8db9f07901d
Joseph Alikah <alikahjoseph@gmail.com>
2022-09-28 14:39:50 +0100

    0x04 - Fix task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,16 @@
 
 def square_matrix_simple(matrix=[]):
     """Compute the square of all values in a matrix."""
-    return [map(lambda x: x ** 2, row) for row in matrix]
+    if matrix is None:
+        return []
+    if len(matrix) == 0:
+        return []
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
+
+
+if __name__ == '__main__':
+    mat = square_matrix_simple([
+        [1, 2, 3],
+        [4, 5, 6],
+    ])
+    print(mat)
