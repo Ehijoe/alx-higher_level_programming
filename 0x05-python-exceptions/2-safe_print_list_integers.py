9cb68c12e8ac862237c54717ca7f5e88b698e5fd
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-10 14:45:01 +0100

    0x05 - Add task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Print and count integers."""
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x integers in my_list."""
+    printed = 0
+    try:
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+            except ValueError:
+                continue
+            printed += 1
+    except IndexError:
+        pass
+    print()
+    return printed
