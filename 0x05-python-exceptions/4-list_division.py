d8474dfdfcc45730220ed800a0502c50bdfb7d7b
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-10 17:14:21 +0100

    0x05 - Add task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,4 +18,5 @@ def list_division(my_list_1, my_list_2, list_length):
                 result.append(val)
     except IndexError:
         print("out of range")
-    return result
+    finally:
+        return result
