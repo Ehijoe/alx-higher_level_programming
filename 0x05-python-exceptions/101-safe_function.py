ba530fad6e119ee37ed88d9d45b9c2a46a21927f
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-11 00:07:20 +0100

    0x05 - Add task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Safe function."""
+import sys
+
+
+def safe_function(fct, *args):
+    """Execute a function safely."""
+    try:
+        return fct(args)
+    except BaseException as err:
+        print("Exception:", err, file=sys.stderr)
+        return None
