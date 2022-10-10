0e8c20e659c6370e76f753f224fdc37d21697725
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-10 16:47:20 +0100

    0x05 - Fix task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -12,7 +12,7 @@ def safe_print_list_integers(my_list=[], x=0):
             except (ValueError, TypeError):
                 continue
             printed += 1
-    except IndexError:
+    except BaseException:
         pass
     print()
     return printed
