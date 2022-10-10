825cfe9f2ad64914196bfb61ea9ff6a3a288c75a
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-11 00:39:38 +0100

    0x05 - Add task 102

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Magic calculation."""
+
+
+def magic_calculation(a, b):
+    """Do something mystical."""
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
+    return result
+
+
+if __name__ == '__main__':
+    import dis
+    dis.dis(magic_calculation)
