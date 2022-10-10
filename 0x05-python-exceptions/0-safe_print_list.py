736f16d6a5c9cf898601d03539307de82583f7f0
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-10 12:34:03 +0100

    0x05 - Fix task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
     printed = 0
     try:
         for i in range(x):
-            printed += 1
             print(my_list[i], end="")
+            printed += 1
     except BaseException:
         pass
     finally:
