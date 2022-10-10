c3ef48321138885338d30209898aa790d1bda1b0
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-10 12:23:18 +0100

    0x05 - Fix task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,13 @@
 
 def safe_print_list(my_list=[], x=0):
     """Print the item at a particular index of a list."""
+    printed = 0
     try:
         for i in range(x):
+            printed += 1
             print(my_list[i], end="")
     except IndexError:
         pass
     finally:
         print()
+        return printed
