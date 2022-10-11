d4696392f117cddd9fb915f8bd21ecd05b873c98
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-11 22:51:30 +0100

    0x06 - Add task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""My first square."""
+
+
+class Square:
+    """An empty square class."""
+
+    def __init__(self, size=0):
+        """Create a square of a particular size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Return the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        """Set the size of the square."""
+        if type(val) is not int:
+            raise TypeError("size must be an integer")
+        if val < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = val
+
+    def area(self):
+        """Return the area of the square."""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Print the square in #'s."""
+        for i in range(self.size):
+            print("#" * self.size)
