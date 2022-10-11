457a0f267d371c6f187aea0f84f1c918c7b5e192
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-11 23:21:11 +0100

    0x06 - Fix task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,10 +49,10 @@ class Square:
 
     def my_print(self):
         """Print the square in #'s."""
-        for i in range(self.position[1]):
-            print()
         if self.size == 0:
             print()
             return
+        for i in range(self.position[1]):
+            print()
         for i in range(self.size):
             print((" " * self.position[0]) + ("#" * self.size))
