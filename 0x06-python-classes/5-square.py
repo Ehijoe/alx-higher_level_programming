66c1425260f25868e1b9e816151cee537224ca87
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-11 22:53:09 +0100

    0x06 - Add task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -29,5 +29,8 @@ class Square:
 
     def my_print(self):
         """Print the square in #'s."""
+        if self.size == 0:
+            print()
+            return
         for i in range(self.size):
             print("#" * self.size)
