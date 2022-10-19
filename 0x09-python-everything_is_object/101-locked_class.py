735654cff90c3060ee3ed3641ab36843ff9c78cb
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-19 14:35:37 +0100

    0x09 - Fix task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,22 +5,4 @@
 class LockedClass:
     """No attributes can be added."""
 
-    def __setattr__(self, name, value):
-        """Check if attribute to be set is valid."""
-        if name != "first_name":
-            raise AttributeError(
-                "'{}' object has no attribute '{}'".format(
-                    self.__class__.__name__, name
-                )
-            )
-        self.__dict__[name] = value
-
-    def __getattribute__(self, name):
-        """Check if attribute to be set is valid."""
-        if name == "dict":
-            raise AttributeError(
-                "'{}' object has no attribute '__dict__'".format(
-                    self.__class__.__name__
-                )
-            )
-        return self.__dict__[name]
+    __slots__ = ["first_name"]
