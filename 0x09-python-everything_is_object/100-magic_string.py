b48f011d6af04f68ce95bddcc18c7481a51f6915
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-19 14:57:01 +0100

    0x09 - Fix task 100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 i = 0
-def magic_string():
-    global i
-    return ", ".join(["BestSchool"] * (i := i + 1))
+magic_string = lambda: (lambda: ((globals().update(i=i + 1)),
+                                 ", ".join(["BestSchool"] * (i))))()[1]
