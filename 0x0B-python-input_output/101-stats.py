17f79572e10ba2ce2425cbecc849f2fad4b197ac
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-25 23:24:59 +0100

    0x0B - Add task 101

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Log parsing."""
+import sys
+import signal
+
+statuses = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+
+
+def exit_program(signo, frame):
+    """Handle the SIGINT signal."""
+    for stat in statuses:
+        print(f"{stat}: {statuses[stat]}")
+
+
+def main():
+    """Parse logs passed in as stdin."""
+    count = 0
+    signal.signal(signal.SIGINT, exit_program)
+    for line in sys.stdin:
+        statuses[line.split()[-2]] += 1
+        count += 1
+        if count == 10:
+            count = 0
+            for stat in statuses:
+                print(f"{stat}: {statuses[stat]}")
+
+    for stat in statuses:
+        print(f"{stat}: {statuses[stat]}")
+
+
+if __name__ == '__main__':
+    main()
