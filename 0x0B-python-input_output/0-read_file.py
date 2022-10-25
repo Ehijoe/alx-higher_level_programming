fb0f1c808646b52abcdaee8cbeaa27d2887d1b95
Joseph Alikah <alikahjoseph@gmail.com>
2022-10-25 21:03:05 +0100

    0x0B - Fix task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,6 @@
 def read_file(filename=""):
     """Print the contents of a file to stdout."""
     with open(filename, "r", encoding="utf-8") as file_:
-        print(file_.read()[:-1])
+        content = file_.read()
+        if content != "":
+            print(content[:-1])
