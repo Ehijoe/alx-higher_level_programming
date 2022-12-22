118559ddcdc72e0d7f56703544c6d93b275a17d6
Joseph Alikah <alikahjoseph@gmail.com>
2022-12-22 22:44:07 +0100

    0x0F - Add task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """Model for the States."""
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
 
 
+engine = create_engine(
+    f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[4]}"
+)
 Base = declarative_base()
 
 
@@ -13,3 +17,6 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+Base.metadata.create_all(engine)
