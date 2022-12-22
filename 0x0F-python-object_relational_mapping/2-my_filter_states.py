236588ccd4e1413d0057b740146281018bff3ffc
Joseph Alikah <alikahjoseph@gmail.com>
2022-12-22 12:58:08 +0100

    0x0F - Add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa."""
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """List the states in the database."""
+    db = MySQLdb.connect(user=argv[1],
+                         password=argv[2],
+                         db=argv[3],
+                         host="localhost",
+                         port=3306)
+    cur = db.cursor()
+    cur.execute(
+        f"SELECT id, name FROM states WHERE name = '{argv[4]}' ORDER BY id ASC;"
+    )
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+
+if __name__ == '__main__':
+    main()
