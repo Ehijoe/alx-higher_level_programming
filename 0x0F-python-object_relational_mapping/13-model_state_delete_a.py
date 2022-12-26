f4bff1a116f494bc787055a7dd8892c7b25670f9
Joseph Alikah <alikahjoseph@gmail.com>
2022-12-26 06:40:28 +0100

    0x0F - Add task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Remove all states with 'a' in them."""
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like("%a%")).all()
+    for state in states:
+        session.delete(state)
+    session.commit()
+    Base.metadata.create_all(engine)
