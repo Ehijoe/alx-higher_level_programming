a49150a55ec4401b834ee98e195277c945e2e31d
Joseph Alikah <alikahjoseph@gmail.com>
2022-12-28 23:51:03 +0100

    0x12 - Add task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const nums = process.argv.slice(2).map(
+  function (arg) {
+    return (parseInt(arg));
+  }
+);
+
+nums.sort();
+
+if (nums.length === 0 || nums.length === 1) {
+  console.log(0);
+} else {
+  console.log(nums[nums.length - 2]);
+}
