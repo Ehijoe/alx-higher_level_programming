4c534df1f93e02f0af57d7c2e2bba69f35d67dd6
Joseph Alikah <alikahjoseph@gmail.com>
2023-01-17 15:46:17 +0100

    0x11 - Fix task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         q = ""
     else:
-        q = sys.argv[0]
+        q = sys.argv[1]
     resp = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
     try:
         user = resp.json()
