bd4559e8e22a64ecbc5dbc71fee809eae43e1211
Joseph Alikah <alikahjoseph@gmail.com>
2023-01-17 12:38:27 +0100

    0x11 - Fix task 0 typo

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
         body = resp.read()
         print("Body response:")
         print(f"\t- type: {type(body)}")
-        print(f"\t- content {body}")
+        print(f"\t- content: {body}")
         print(f"\t- utf8 content: {body.decode('utf-8')}")
