4a5ffa19af6bc6d41c45bf4722522181688cf2df
Joseph Alikah <alikahjoseph@gmail.com>
2023-01-17 14:24:51 +0100

    0x11 - Add task 6

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Send data with a request."""
-import requests
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
 import sys
 
 if __name__ == '__main__':
-    data = {"email": sys.argv[2]}
-    resp = requests.post(sys.argv[1], data=data)
-    print(resp.text)
+    data = urlencode({"email": sys.argv[2]}).encode("ascii")
+    req = Request(sys.argv[1], data=data)
+    with urlopen(req) as resp:
+        print(resp.read().decode("utf-8"))
