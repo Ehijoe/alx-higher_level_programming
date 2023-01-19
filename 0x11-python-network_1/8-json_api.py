1547c5eadca9d2744d06db2a80dc90cbbd507e80
Joseph Alikah <alikahjoseph@gmail.com>
2023-01-19 10:23:15 +0100

    0x11 - Fix task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Search for a name using an api."""
 import requests
-from requests.exceptions import JSONDecodeError
+import requests.exceptions.JSONDecodeError
 import sys
 
 if __name__ == '__main__':
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     resp = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
     try:
         user = resp.json()
-    except JSONDecodeError:
+    except requests.exceptions.JSONDecodeError:
         print("Not a valid JSON")
     if type(user) is not dict or "id" not in user or "name" not in user:
         print("No result")
